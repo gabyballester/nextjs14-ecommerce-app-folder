f30aef155ff7aa558b9bc5369a9268e1c08026f8
gabyballester <gaby.ballester@gmail.com>
2024-06-12 21:48:53 +0200

    Setup shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,84 @@
-import Link from 'next/link';
-import Image from 'next/image';
+// import Link from "next/link";
+// import Image from "next/image";
+
+// import * as React from "react";
+
+import { ThemeToggle } from "@/components/theme-toggle";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export default function Home() {
   return (
-    <main className="m-6 flex min-h-screen items-center justify-center">
-      <Image
-        className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-        src="/next.svg"
-        alt="Next.js Logo"
-        width={180}
-        height={37}
-        priority
-      />
-      <Link href="/about">Click here</Link>
-    </main>
+    <section className="py-24">
+      <div className="container">
+        <h1 className="text-3xl font-bold">NextJs Boilerplate app folder</h1>
+
+        <ThemeToggle />
+
+        <Card className="mt-12 w-[350px]">
+          <CardHeader>
+            <CardTitle>Create project</CardTitle>
+            <CardDescription>
+              Deploy your new project in one-click.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="grid w-full items-center gap-4">
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="name">Name</Label>
+                  <Input id="name" placeholder="Name of your project" />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="framework">Framework</Label>
+                  <Select>
+                    <SelectTrigger id="framework">
+                      <SelectValue placeholder="Select" />
+                    </SelectTrigger>
+                    <SelectContent position="popper">
+                      <SelectItem value="next">Next.js</SelectItem>
+                      <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                      <SelectItem value="astro">Astro</SelectItem>
+                      <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex justify-between">
+            <Button variant="outline">Cancel</Button>
+            <Button>Deploy</Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </section>
+    // <main className="m-6 flex min-h-screen items-center justify-center">
+    //   <Image
+    //     className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+    //     src="/next.svg"
+    //     alt="Next.js Logo"
+    //     width={180}
+    //     height={37}
+    //     priority
+    //   />
+    //   <Link href="/about">Click here</Link>
+    // </main>
   );
 }
